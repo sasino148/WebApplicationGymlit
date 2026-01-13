6492fb9ba457fc372ea31e041991c39ddc649d99
sasa prucha <pruchasasa@gmail.com>
2026-01-13 16:08:46 +0100

    commit dalsi

--- src/main.js
+++ src/main.js
@@ -12,22 +12,39 @@ let counter = 0;
 hiButton.innerHTML = `${counter}`;
 
 const testDiv = document.createElement("div");
-testDiv.style.border = "solid 2px";
-testDiv.style.width = "100px";
+testDiv.classList.add("roundObject");
+testDiv.style.width = "200px";
 testDiv.style.height = "200px";
 testDiv.style.left = "200px";
 testDiv.style.top = "200px";
 testDiv.style.position = "absolute";
-
 world.appendChild(testDiv);
 
-hiButton.addEventListener("click", (e) => {
-    ++counter;
-    hiButton.innerHTML = `${counter}`;
-    if(counter === "1"){
-        hiButton.style.background = "red";
-    } else {
-        hiButton.style.background = "green";
+const round = document.createElement("div");
+round.classList.add("round");
+round.style.width = "70px";
+round.style.height = "70px";
+round.style.left = "70px";
+round.style.top = "-35px";
+round.style.position = "absolute";
+testDiv.appendChild(round);
+
+const round2 = document.createElement("div");
+round2.classList.add("round");
+round2.style.width = "70px";
+round2.style.height = "70px";
+round2.style.left = "70px";
+round2.style.top = "165px";
+round2.style.position = "absolute";
+testDiv.appendChild(round2);
+
+
+    round.addEventListener("mousedown", (e) => {
+    if(e.button === 0){
+        round.style.background = "red";
+    } else if(e.button === 2){
+        round.style.background = null
     }
 
+    
 });
